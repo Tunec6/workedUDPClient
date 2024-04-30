6ec6cd50fdefa802a89c5a7befaceed297348ef0
Tunec <112854325+Tunec6@users.noreply.github.com>
2024-04-30 13:46:49 +0500

    Update Form1.cs

--- UDPClient/Form1.cs
+++ UDPClient/Form1.cs
@@ -1,124 +1,123 @@
 using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Timers;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace udpClient
 {
     public partial class Form1 : Form
     {
-        private static IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.220.242"), 35488);
+        private static IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.220.237"), 35488);
         private static UdpClient udpClient = new UdpClient(34285);
-        private static string ipFrom = "192.168.220.242";
+        private static string ipFrom = "192.168.220.237";
         private static string ipTo = "";
         private System.Threading.Timer _timer = null;
+
+        private static MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase database = mongoClient.GetDatabase("ChatDB");
+        private static IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("chats");
+
         public Form1()
         {
-            String strHostName = string.Empty;
-            strHostName = Dns.GetHostName();
-            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
-            IPAddress[] addr = ipEntry.AddressList;
-
             InitializeComponent();
 
             _timer = new System.Threading.Timer(TimerCallback, null, 0, 10000);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-
-
-
             Message message = new Message(Command.Text, Text.Text, ipTo, ipFrom);
             string messageJson = message.ToJSON();
             byte[] messageData = Encoding.UTF8.GetBytes(messageJson);
+            await DisplayMessage();
             udpClient.Send(messageData, ep);
-            Updater();
-
-            //var curDir = Directory.GetCurrentDirectory();
-            //curDir = curDir.Replace("\\", "/");
-            //string filePath = $"{curDir}/history/{ipFrom}.txt";
-            //string content = File.ReadAllText(filePath);
-            //textBox1.Text = content;
 
         }
 
-        private static void TimerCallback(Object o)
+        public async Task DisplayMessage()
         {
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var cursor = await collection.FindAsync(filter);
+            cursor.ForEachAsync(document =>
+            textBox1.AppendText(document.ToString() + Environment.NewLine));
+            _timer = new System.Threading.Timer(TimerCallback, null, 0, 10000);
+        }
+
+        private static async void TimerCallback(Object o)
+        {
+            //Form1 form = (Form1)o;
+            //await form.DisplayMessage();       
+
             if (ipTo != "")
             {
                 Message message = new Message("Update", "", ipTo, ipFrom);
                 string messageJson = message.ToJSON();
-
                 byte[] messageData = Encoding.UTF8.GetBytes(messageJson);
                 udpClient.Send(messageData, ep);
+                
             }
-
         }
 
-        private async void Updater()
+        public void UpdateMessage()
         {
+            ipTo = IpToBox.Text;
+            string modIpTo = ModIP(ipTo);
+            string modIpFrom = ModIP(ipFrom);
+            string path = GenerateFileName(modIpFrom, modIpTo);
             var curDir = Directory.GetCurrentDirectory();
             curDir = curDir.Replace("\\", "/");
-            string newFileName = String.Format($"{curDir}/history/{ipFrom + " = " + ipTo}.txt");
-            if (File.Exists(newFileName))
-            {
-                string fileText = File.ReadAllText(newFileName);
-                textBox1.Text = fileText;
-            }
+            string newFileName = String.Format($"{curDir}/history/{path}.txt");
         }
 
-        public static async Task ClientListner()
+        public async Task ClientListener()
         {
             while (true)
             {
-                var curDir = Directory.GetCurrentDirectory();
-                curDir = curDir.Replace("\\", "/");
-                string newFileName = String.Format($"{curDir}/history/{ipFrom + "=" + ipTo}.txt");
                 var receiveResult = await udpClient.ReceiveAsync();
                 byte[] answerData = receiveResult.Buffer;
-                File.WriteAllBytes(newFileName, answerData);
 
-
+                string message = Encoding.UTF8.GetString(answerData);
+                textBox1.Invoke((MethodInvoker)delegate
+                {
+                    textBox1.AppendText(message + Environment.NewLine);
+                });
             }
-
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
             ipTo = IpToBox.Text;
-            await ClientListner();
-            Updater();
+            UpdateMessage();
 
+            await ClientListener();
         }
 
-        public static string ModIP(string adres)
+        public static string ModIP(string address)
         {
-            adres = adres.Replace(".", "_");
+            address = address.Replace(".", "_");
             string modIp = "";
 
-            for (int i = 0; i < adres.Length; i++)
+            for (int i = 0; i < address.Length; i++)
             {
-                if (adres[i] == ':')
+                if (address[i] == ':')
                 {
-                    modIp = adres.Substring(0, i);
+                    modIp = address.Substring(0, i);
                 }
             }
             if (modIp == "")
             {
-                modIp = adres;
+                modIp = address;
             }
             return modIp;
-
-
         }
 
         public static string GenerateFileName(string firstIp, string secondIp)
         {
-
             string[] arr1 = firstIp.Split('_');
             string[] arr2 = secondIp.Split('_');
             for (int i = 0; i < 4; i++)
@@ -133,11 +132,15 @@ namespace udpClient
                 }
             }
             return null;
+        }
 
+        
+        private void Command_TextChanged(object sender, EventArgs e)
+        {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
